ee4104512d3d5787d10140438c8ca9186c559e85
Yury Torgashin <imaginarylight@yandex.ru>
2023-05-07 22:02:01 +0300

    customize range input

--- src/components/UI/range/range.tsx
+++ src/components/UI/range/range.tsx
@@ -1,9 +1,19 @@
-import React, {FC, InputHTMLAttributes} from 'react';
+import React, {FC, InputHTMLAttributes, useEffect, useRef} from 'react';
 import styles from './range.module.scss'
 
 const Range: FC<InputHTMLAttributes<HTMLInputElement>> = ({...props}) => {
+    const rangeRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        const percentage = Math.round(props.value as number / 20 * 100);
+        if (rangeRef.current) {
+            rangeRef.current.style.background = `linear-gradient(to right, #A4FFAF ${percentage}%, #18171F ${percentage}%)`
+        };
+    }, [props.value])
+
+
     return (
-        <input type="range" className={styles.range} {...props}>
+        <input type="range" ref={rangeRef} className={styles.range} {...props}>
 
         </input>
     );
