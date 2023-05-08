be6a632f68648863a4de705e45799efb7cfaaf60
Yury Torgashin <imaginarylight@yandex.ru>
2023-05-08 12:32:04 +0300

    fix mobile

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import App from './components/app/app'
-import AppContext from "./context";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
